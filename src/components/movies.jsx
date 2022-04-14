c3cd979cd9f7e71a3427f6fc64b79302771bfcd3
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2022-04-14 22:30:04 +0530

    add like button

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
-import {getMovies, deleteMovie} from "../services/fakeMovieService";
-
+import Like from "./common/like";
+import {getMovies} from "../services/fakeMovieService";
 
 export default class Movies extends Component {
     state = {
@@ -10,6 +10,12 @@ export default class Movies extends Component {
     handleDeleteMovie = (id) => {
         const movies = this.state.movies.filter(movie => movie._id !== id);
         this.setState({movies});
+    }
+
+    likeMovie = (id) => {
+        const movieIndex = this.state.movies.findIndex(movie => movie._id === id);
+        this.state.movies[movieIndex].liked = !this.state.movies[movieIndex].liked;
+        this.setState({movies: this.state.movies});
     }
 
     render() {
@@ -28,6 +34,7 @@ export default class Movies extends Component {
                         <th>Stock</th>
                         <th>Rate</th>
                         <th></th>
+                        <th></th>
                     </tr>
                     </thead>
                     <tbody>
@@ -37,6 +44,11 @@ export default class Movies extends Component {
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <Like liked={movie.liked} onClick={() => {
+                                    this.likeMovie(movie._id)
+                                }}/>
+                            </td>
                             <td>
                                 <button onClick={() => this.handleDeleteMovie(movie._id)}
                                         className="btn btn-danger btn-sm">Delete
